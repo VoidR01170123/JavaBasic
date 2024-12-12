e7e18f2ec71e13d3ec01dee5ff74814aef589db4
VoidR01170123 <asj01170123@gmail.com>
2024-12-12 23:35:46 +0900

    third commit

---
+++ src/variable/Var5.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        //1. Declaring variable and Initializing separately
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        int b = 2; //2. Declaring variable and Initializing tgt
+        System.out.println(b);
+
+        int c = 3, d = 4; //3. Declaring numerous variables and Initializing tgt
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+}
---
+++ src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //Integer
+        double b = 10.5; // Real number
+        boolean c = true; // Boolean (only true/false can be put)
+        char d = 'A'; // Only one letter
+        String e = "Hello Java"; // String, special type for String
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
