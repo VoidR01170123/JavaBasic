bf8e4b4d9df59bd5a0da50515b73c2b65321707f
VoidR01170123 <asj01170123@gmail.com>
2024-12-16 21:13:21 +0900

    2024_12_16

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,10 @@
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println((2*(num++)));
+        }
+    }
+}
---
+++ src/loop/ex/WhileEx3.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+        int i = 1, sum = 0, max = 3;
+        while (i <= max) {
+            sum += i;
+            i++;
+        }
+        System.out.println("sum=" + sum);
+    }
+}
