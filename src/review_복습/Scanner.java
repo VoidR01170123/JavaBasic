366c0864283a8333d6cf2d25bd042810490fc480
VoidR01170123 <asj01170123@gmail.com>
2025-02-16 17:04:35 +0900

    2025_02_16

---
+++ src/review_복습/Scanner.java
@@ -0,0 +1,15 @@
+package review_복습;
+
+/*
+
+Scanner scanner = new Scanner(System.in);
+
+scanner.nextLine(); // 입력을 String으로 가져옴
+scanner.nextInt(); // 입력을 int로 가져옴
+scanner.nextDouble(); // 입력을 double로 가져옴
+
+Scanner scanner 로 scanner라고 하는 Scanner 타입의 변수를 선언함. new Scanner()를 통해 메모리를 할당하고
+System.in으로 입력받음.
+
+
+ */
